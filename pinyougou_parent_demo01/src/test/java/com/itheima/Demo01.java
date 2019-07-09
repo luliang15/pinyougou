399569d1fae7ded80234f93612585ff8a23fd5ef
luliang <1575190048@qq.com>
2019-07-09 12:38:40 +0800

    品优购

---
+++ pinyougou_parent_demo01/src/test/java/com/itheima/Demo01.java
@@ -0,0 +1,13 @@
+package com.itheima;
+
+/**
+ * @program:PinYouGou01
+ * @description:测试
+ * @author:Mr.lu
+ * @create:2019-07-09 12:27
+ **/
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("你好啊");
+    }
+}
