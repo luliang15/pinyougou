abae11f9630a77af3b00201b30cfa069fa3e8064
luliang <1575190048@qq.com>
2019-07-31 15:10:17 +0800

    购物车的添加需求实现

---
+++ pinyougou_parent_demo01/pinyougou_pojo/src/main/java/com/pinyougou/pojogroup/Cart.java
@@ -0,0 +1,50 @@
+package com.pinyougou.pojogroup;
+
+import com.pinyougou.pojo.TbOrderItem;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @Description:luliang
+ * @Project:购物车的实体类
+ * @CreateDate: Created in 2019-07-31 11:08
+ */
+public class Cart implements Serializable {
+    private String sellerId;//商家Id
+    private String sellerName;//商家名称
+    private List<TbOrderItem> orderItemList;//购物车明细
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getSellerName() {
+        return sellerName;
+    }
+
+    public void setSellerName(String sellerName) {
+        this.sellerName = sellerName;
+    }
+
+    public List<TbOrderItem> getOrderItemList() {
+        return orderItemList;
+    }
+
+    public void setOrderItemList(List<TbOrderItem> orderItemList) {
+        this.orderItemList = orderItemList;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "sellerId='" + sellerId + '\'' +
+                ", sellerName='" + sellerName + '\'' +
+                ", orderItemList=" + orderItemList +
+                '}';
+    }
+}
